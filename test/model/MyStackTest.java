7788f53d462207fd125d737158042c313a6efcfc
Sinis2101 <sinisterra2101@gmail.com>
2021-09-23 12:07:54 -0500

    Unit Test for MyStack class. First case implemented to verify if a stack is successfully created.

---
+++ test/model/MyStackTest.java
@@ -0,0 +1,20 @@
+package model;
+
+import MyStack_data_structure.MyStack;
+import junit.framework.TestCase;
+
+public class MyStackTest extends TestCase {
+
+    public void sc1() {
+    }
+
+    public void testMyStack(){ //Test MyStack constructor.
+        sc1();
+
+        MyStack<Integer> stack = new MyStack<>();
+
+        assertTrue(stack.getArray().isEmpty()); //Checks if the arraylist is empty.
+        assertEquals(-1, stack.getTop()); //Checks if top value is -1.
+    }
+
+}
