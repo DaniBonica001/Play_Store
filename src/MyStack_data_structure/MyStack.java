716b035f8d831178e913ef2e1b18baf06ac3a65a
Sinis2101 <sinisterra2101@gmail.com>
2021-09-23 12:08:25 -0500

    Getters added and fixed constructor.

--- src/MyStack_data_structure/MyStack.java
+++ src/MyStack_data_structure/MyStack.java
@@ -5,15 +5,13 @@ import java.util.*;
 public class MyStack<T> implements MyStackInterface<T>{
 
 	//Atributes
-	private ArrayList<T> A; // Empty array list
-	private int top = -1; // Default value of top variable when stack is empty
-	//int size;// Variable to store size of array
+	private ArrayList<T> array; // Empty array list
+	private int top; // Default value of top variable when stack is empty is -1
 
 	//Constructor
 	public MyStack(){
-		//this.size = size; // Storing the value of size into global variable
-		this.A = new ArrayList<T>();// Creating array of Size = size
-		//this.top=-1;// Default value of top variable when stack is empty
+		this.array = new ArrayList<T>();// Creating array of Size = size
+		this.top = -1;// Default value of top variable when stack is empty
 	}
 
 	public void setTop(int top) {
@@ -26,14 +24,14 @@ public class MyStack<T> implements MyStackInterface<T>{
 
 	//Method to push generic element into stack
 	public void push(T element){
-		A.add(element);
+		array.add(element);
 		top += 1;		
 	}
 	
 	// Method to return the top of the stack without deleting it
 	public T peek() {
 		if(top>-1) {//Si el stack no está vacio
-			return A.get(top);
+			return array.get(top);
 		}else {
 			return null;
 		}
@@ -46,8 +44,8 @@ public class MyStack<T> implements MyStackInterface<T>{
 			return null;
 		}
 		else {
-			T object=A.get(top);//guardo el que era el top y el que se va a devolver
-			A.remove(top);//borro el elemento que estaba en el top
+			T object= array.get(top);//guardo el que era el top y el que se va a devolver
+			array.remove(top);//borro el elemento que estaba en el top
 			top -= 1;//muevo el top hacia abajo para indicar que el que le seguía ahora es el top
 			//setTop(top-1);
 			return object;
@@ -68,17 +66,19 @@ public class MyStack<T> implements MyStackInterface<T>{
 	// @Override
 	public String toString(){
 		String Ans = " ";
-		for (int i = 0; i <A.size(); i++) {		
+		for (int i = 0; i < array.size(); i++) {
 			
-			if (i==A.size()-1) {
-				Ans += "Top: "+String.valueOf(A.get(i).toString());
+			if (i== array.size()-1) {
+				Ans += "Top: "+String.valueOf(array.get(i).toString());
 			}else {
-				Ans += String.valueOf(A.get(i).toString()) + "-> ";
+				Ans += String.valueOf(array.get(i).toString()) + "-> ";
 			}
 			
 		}
 		return Ans;
 	}
-	
-	
+
+	public ArrayList<T> getArray() {
+		return array;
+	}
 }
